8cd8c52f8a762f30e7cb5826de67130d970e6ff3
stephanie mairena <stefyem7711@gmail.com>
2020-08-25 20:30:45 -0400

    STRETCH

--- server.js
+++ server.js
@@ -51,6 +51,48 @@ server.post('/cars', async(req,res) => {
     }
 })
 
+//stretch
+
+
+server.put('/cars/:id', async(req,res) => {
+    try {
+        await carsdb('cartable')
+        .update({
+            vin:req.body.vin,
+            make:req.body.make,
+            model:req.body.model,
+            mileage:req.body.mileage,
+            transmission_type:req.body.transmission_type,
+            status:req.body.status
+        })
+        .where('id', req.params.id)
+        
+        const car = await carsdb('cartable')
+            .where('id', req.params.id)
+            .first()
+
+            res.json(car)
+
+       } catch (err) {
+           console.log(err)
+       }
+        
+})
+
+server.delete('/cars/:id', async(req,res) => {
+    try {
+       await carsdb('cartable')
+        .where('id', req.params.id)
+        .del()
+        res.status(204).end()
+
+       } catch (err) {
+           console.log(err)
+       }
+        
+})
+
+
 server.get('/', (req,res) => {
     res.send(`<h1>API is up!</h1>`)
 });
